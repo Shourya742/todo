887ba35d1b1e07c10c68385a02e7cfe3b8c53fa5
Shourya742 <sshourya17@gmail.com>
2023-08-01 20:01:20 +0530

    Done with frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Router, Routes, useNavigate } from "react-router-dom";
+import { RecoilRoot, useSetRecoilState } from "recoil";
+import { authState } from "./store/authState";
+import { useEffect } from "react";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import TodoList from "./components/TodoList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RecoilRoot>
+        <Router>
+          <InitState />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/todos" element={<TodoList />} />
+          </Routes>
+        </Router>
+      </RecoilRoot>
     </>
-  )
+  );
 }
 
-export default App
+function InitState() {
+  const setAuth = useSetRecoilState(authState);
+  const navigate = useNavigate();
+  const init = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch("http://localhost:3000/auth/me", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const data = await response.json();
+      if (data.username) {
+        setAuth({ token: data.token, username: data.username });
+        navigate("/todos");
+      } else {
+        navigate("/login");
+      }
+    } catch (error) {
+      navigate("/login");
+    }
+  };
+  useEffect(() => {
+    init();
+  }, []);
+  return <></>;
+}
+
+export default App;
